57a8e5720d5fa5c486774b85cf7b8a0de927a062
GFou <gfouchard20@outlook.com>
2022-11-06 17:59:37 -0500

    create skills components & add program icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import './App.css';
 import { Title } from './components/Title/Title';
 import { NavBar } from './components/NavBar/NavBar';
+import { AboutMe } from './components/AboutMe/AboutMe';
+import { Skills } from './components/Skills/Skills';
 
 function App() {
   return (
     <div>
       <Title />
       <NavBar />
+      <AboutMe />
+      <Skills />    
     </div>
   )
 }
---
+++ src/components/AboutMe/AboutMe.jsx
@@ -0,0 +1,10 @@
+export function AboutMe() {
+  return (
+    <div className="about-container">
+      <h3>Hi,my name is</h3>
+      <h2>Gregory</h2>
+      <h3>I love learning new skills, and creating user-friendly applications.</h3>
+      <p>I am a Front End Engineer with a passion to provide an exceptional experience</p>
+    </div>
+  )
+}
