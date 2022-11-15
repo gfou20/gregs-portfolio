e3d567b92436f5d93a1aeeb278326c63cacaeb2f
GFou <gfouchard20@outlook.com>
2022-11-15 15:54:12 -0500

    add carousel bootstrap view for projects

---
+++ src/data/projects.js
@@ -0,0 +1,25 @@
+const projects =[
+  {
+    title: '90s Quiz Game',
+    description: 'Quiz app testing players on their 90s cartoon knowledge',
+    image: './images/90s-quiz.jpg',
+    repositoryLink: 'https://github.com/gfou20/90s-Quiz-Game.git', 
+    delpoymentLink: 'https://90s-cartoon-quiz.netlify.app/'
+  },
+  {
+    title: 'Get Pumped Tracker',
+    description: 'Tracker app that allows users to set fitness goals and keep track of completed workouts',
+    image: './images/get-pumped-tracker.jpg',
+    repositoryLink: 'https://github.com/gfou20/get-pumped-tracker.git',
+    delpoymentLink: 'https://get-pumped-tracker.fly.dev/'
+  },
+  {
+    title: 'Sneaker Collector',
+    description: 'App that allows users to add or edit sneakers added to their collection.',
+    image: './images/sneaker-collector.png',
+    repositoryLink: 'https://github.com/gfou20/sneaker-collector.git',
+    delpoymentLink: 'https://sneaker-collector.herokuapp.com/'
+  },
+]
+
+export default projects
--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,7 +1,51 @@
+import Carousel from 'react-bootstrap/Carousel'
+
 function Projects() {
-  return(
-      <h1>Projects</h1>
-  )
+  return (
+    <Carousel fade>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src='./images/90s-quiz.jpg'
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>90s Quiz Game</h3>
+          <p>An app that allows the users to select a quiz and answer a number of questions before time runs out.</p>
+          <p>Javascript, HTML, CSS</p>
+          <hr />
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src='./images/get-pumped-tracker.jpg'
+          alt="Second slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Get Pumped Tracker</h3>
+          <p>An app that allows the user to track their fitness routines. The user is able to record how many sets/reps, the date of the workout as well as view other users fitness routines.</p>
+          <p>MongoDB, Javascript, HTML, CSS</p>
+          <hr />
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src='./images/sneaker-collector.png'
+          alt="Third slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Sneaker Collector</h3>
+          <p>An app that allows the user to add collected sneakers and track where these sneakers were purchased. User is able to edit sneaker price, location they were purchased and sneaker details.</p>
+          <p>Postgres SQL, Django, Javascript, HTML, CSS</p>
+          <hr />
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
 }
 
 export default Projects
