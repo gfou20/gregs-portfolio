d322cd6d822d2ec1be6a955391c9f2047d683409
GFou <gfouchard20@outlook.com>
2022-11-16 14:04:10 -0500

    edit about css

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -7,6 +7,7 @@ function About() {
         <h1>About</h1>
         <img src='./images/greg.jpg' alt='portfolio' />
         <p>I am a driven software engineer using my experience in customer service and health administration to deliver a smooth and impactful user-friendly experience. I thrive in dynamic work environments and am always looking for new opportunities to learn and grow.</p>
+        <hr />
       </main>
       <section className={styles.interests}>
         <h2>Interests</h2>
