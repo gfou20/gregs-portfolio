0accbcd232abca66001a24dbcf8409856f392fa2
GFou <gfouchard20@outlook.com>
2022-11-16 12:00:28 -0500

    css edit to about

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,10 +1,23 @@
+import styles from './About.module.css'
+
 function About() {
   return (
-    <div>
-      <h1>About</h1>
-      <img src='./images/greg.jpg' alt='portfolio' />
-      <p>I am a driven software engineer using my experience in customer service and health administration to deliver a smooth and impactful user-friendly experience.</p>
-    </div>
+    <>
+      <main className={styles.about}>
+        <h1>About</h1>
+        <img src='./images/greg.jpg' alt='portfolio' />
+        <p>I am a driven software engineer using my experience in customer service and health administration to deliver a smooth and impactful user-friendly experience. I thrive in dynamic work environments and am always looking for new opportunities to learn and grow.</p>
+      </main>
+      <section className={styles.interests}>
+        <h2>Interests</h2>
+        <ul>
+          <li>Exercising</li>
+          <li>Reading manga/Watching anime</li>
+          <li>Coding</li>
+          <li>Gaming</li>
+        </ul>
+      </section>
+    </>
   )
 }
 
