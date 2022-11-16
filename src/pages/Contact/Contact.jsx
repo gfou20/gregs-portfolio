4c2553958cf915e65e8e6063845b92b5dd506507
GFou <gfouchard20@outlook.com>
2022-11-16 14:19:04 -0500

    edit contact css

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,10 +1,14 @@
+import styles from './Contact.module.css'
+
 function Contact() {
   return (
-    <div>
-      <h1>Contact Me</h1>
-      <p>Please reach out for further information via <a href='mailto:gfouchard20@outlook.com.com'>email</a>.</p>
-      <p>You can view my <a href='https://www.linkedin.com/in/gfouchard/' target='_blank' rel='noreferrer'>LinkedIn</a> or <a href='https://github.com/gfou20' target='_blank' rel='noreferrer'>Github</a> profiles for more.</p>
-    </div>
+    <>
+      <main className={styles.contact}>
+        <h1>Contact Me</h1>
+        <p>Please reach out for further information via <a href='mailto:gfouchard20@outlook.com.com'>email</a>.</p>
+        <p>You can view my <a href='https://www.linkedin.com/in/gfouchard/' target='_blank' rel='noreferrer'>LinkedIn</a> or <a href='https://github.com/gfou20' target='_blank' rel='noreferrer'>Github</a> profiles for more.</p>
+      </main>
+    </>
   )
 }
 
