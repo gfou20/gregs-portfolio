bf48da18ba44c130b3f0492f925a696a1a7ff556
GFou <gfouchard20@outlook.com>
2022-11-18 11:32:08 -0500

    home css edits/hover add

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,17 +1,19 @@
+import { Link } from "react-router-dom"
 import { Skill } from '../Skill/Skill'
 import styles from './Home.module.css'
 
 function Home() {
   return (
-    <>
-      <header className={styles.header}>
-        <h1>Gregory Fouchard | Front End Engineer</h1>
-        <img src='./images/greg.jpg' alt='portfolio' className={styles.home} />
+    <div className={styles.c1}>
+      <header className={styles.main}>
+        <h1>Gregory Fouchard</h1>
+        <h2>Software Engineer</h2>
+        <Link to='/about'><img src='./images/greg.jpg' alt='portfolio' className={styles.home} /></Link>
       </header>
       <section className={styles.skills}>
         <Skill />
       </section>
-    </>
+    </div>
   )
 }
 
