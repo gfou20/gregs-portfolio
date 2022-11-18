fe7beca7c9655dad91adc3ac3a1d7b084349be18
GFou <gfouchard20@outlook.com>
2022-11-18 12:18:50 -0500

    contact and font family edits to app css

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -7,6 +7,7 @@ function Contact() {
         <h1>Contact Me</h1>
         <p>Please reach out for further information via <a href='mailto:gfouchard20@outlook.com.com'>email</a>.</p>
         <p>You can view my <a href='https://www.linkedin.com/in/gfouchard/' target='_blank' rel='noreferrer'>LinkedIn</a> or <a href='https://github.com/gfou20' target='_blank' rel='noreferrer'>Github</a> profiles for more.</p>
+        <button><a href='./images/Gregory_Fouchard_Resume.pdf' download className={styles.download}>Download Resume</a></button>
       </main>
     </>
   )
