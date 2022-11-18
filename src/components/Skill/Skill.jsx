b474293def888c1d855d3cdcc9c0028d5b53bceb
GFou <gfouchard20@outlook.com>
2022-11-18 17:04:23 -0500

    edit skills css

--- src/components/Skill/Skill.jsx
+++ src/components/Skill/Skill.jsx
@@ -1,21 +1,33 @@
+import './Skill.module.css'
 import styles from './Skill.module.css'
 
 export function Skill() {
   return(
-    <section className={styles.skills}>
-      <h1>Tech Skills</h1>
-      <i class="devicon-bootstrap-plain-wordmark"></i>
-      <i class="devicon-react-original-wordmark"></i>
-      <i class="devicon-javascript-plain"></i>
-      <i class="devicon-python-plain"></i>
-      <i class="devicon-html5-plain-wordmark"></i>
-      <i class="devicon-css3-plain-wordmark"></i>
-      <i class="devicon-django-plain-wordmark"></i>
-      <i class="devicon-docker-plain-wordmark"></i>
-      <i class="devicon-express-original-wordmark"></i>
-      <i class="devicon-nodejs-plain-wordmark"></i>
+    <>
+      <div className={styles.set1}>
+        <i class="devicon-react-original-wordmark"></i>
+        <i class="devicon-javascript-plain"></i>
+      </div>
+      <div className={styles.set2}>
+        <i class="devicon-python-plain"></i>
+        <i class="devicon-django-plain-wordmark"></i>
+      </div>
+      <div className={styles.set3}>
+        <i class="devicon-html5-plain-wordmark"></i>
+        <i class="devicon-css3-plain-wordmark"></i>
+      </div>
+      <div className={styles.set4}>
       <i class="devicon-mongodb-plain-wordmark"></i>
       <i class="devicon-postgresql-plain-wordmark"></i>
-    </section>
+      </div>
+      <div className={styles.set5}>
+      <i class="devicon-express-original-wordmark"></i>
+      <i class="devicon-nodejs-plain-wordmark"></i>
+      </div>
+      <div className={styles.set6}>
+        <i class="devicon-bootstrap-plain-wordmark"></i>
+        <i class="devicon-docker-plain-wordmark"></i>
+      </div>
+    </>
   )
 }
